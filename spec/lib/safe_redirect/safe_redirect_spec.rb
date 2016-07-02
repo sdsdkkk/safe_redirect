be854b2de08171f9b0e0184ad11fc196a5abe7a1
sdsdkkk <vcc.edwint@gmail.com>
2016-07-02 10:10:27 +0700

    refactor specs

--- spec/lib/safe_redirect/safe_redirect_spec.rb
+++ spec/lib/safe_redirect/safe_redirect_spec.rb
@@ -10,78 +10,29 @@ module SafeRedirect
       load_config
     end
 
-    it "considers https://www.bukalapak.com a safe domain" do
-      expect(Controller.safe_domain?('https://www.bukalapak.com')).to eq(true)
+    SAFE_PATHS = [
+      'https://www.bukalapak.com',
+      '/',
+      'http://www.twitter.com',
+      :back,
+      {controller: 'home', action: 'index'}
+    ]
+
+    UNSAFE_PATHS = %w(// https://www.bukalapak.com@google.com http://////@@@@@@attacker.com//evil.com
+                      .@@@google.com //bukalapak.com%25%40%25%40%25%40%25%40%25%40%25%40%25%40evil.com
+                      %25%40%25%40%25%40%25%40%25%40%25%40%25%40%25%40%25%40%25%40evil.com 
+                      %@%@%@%@%@%@%@%@%@%@evil.com https://www-bukalapak.com)
+
+    SAFE_PATHS.each do |path|
+      it "considers #{path} a safe path" do
+        expect(Controller.safe_path(path)).to eq(path)
+      end
     end
 
-    it "considers / a safe domain" do
-      expect(Controller.safe_domain?('/')).to eq(true)
-    end
-
-    it "considers // an unsafe domain" do
-      expect(Controller.safe_domain?('//')).to eq(false)
-    end
-
-    it "considers http://www.twitter.com a safe domain" do
-      expect(Controller.safe_domain?('http://www.twitter.com')).to eq(true)
-    end
-
-    it "considers https://www.bukalapak.com@google.com an unsafe domain" do
-      expect(Controller.safe_domain?('https://www.bukalapak.com@google.com')).to eq(false)
-    end
-
-    it "considers https://www.bukalapak.com a safe path" do
-      expect(Controller.safe_path('https://www.bukalapak.com')).to eq('https://www.bukalapak.com')
-    end
-
-    it "considers / a safe path" do
-      expect(Controller.safe_path('/')).to eq('/')
-    end
-
-    it "considers // an unsafe path" do
-      expect(Controller.safe_path('//')).to eq('/')
-    end
-
-    it "considers http://www.twitter.com a safe path" do
-      expect(Controller.safe_path('http://www.twitter.com')).to eq('http://www.twitter.com')
-    end
-
-    it "considers :back a safe path" do
-      expect(Controller.safe_path(:back)).to eq(:back)
-    end
-
-    it "considers {controller: 'home', action: 'index'} a safe path" do
-      expect(Controller.safe_path({controller: 'home', action: 'index'})).to eq({controller: 'home', action: 'index'})
-    end
-
-    it "considers https://www.bukalapak.com@google.com an unsafe path" do
-      expect(Controller.safe_path('https://www.bukalapak.com@google.com')).to eq('/')
-    end
-
-    it "considers .@@@google.com an unsafe path" do
-      expect(Controller.safe_path('.@@@google.com')).to eq('/')
-      expect(Controller.safe_path('.@@@google.com/search')).to eq('/search')
-    end
-
-    it "considers http://////@@@@@@attacker.com//evil.com an unsafe path" do
-      expect(Controller.safe_path('http://////@@@@@@attacker.com//evil.com')).to eq('/')
-    end
-
-    it "considers //bukalapak.com%25%40%25%40%25%40%25%40%25%40%25%40%25%40evil.com an unsafe path" do
-      expect(Controller.safe_path('//bukalapak.com%25%40%25%40%25%40%25%40%25%40%25%40%25%40evil.com')).to eq('/')
-    end
-
-    it "considers %25%40%25%40%25%40%25%40%25%40%25%40%25%40%25%40%25%40%25%40evil.com an unsafe path" do
-      expect(Controller.safe_path('%25%40%25%40%25%40%25%40%25%40%25%40%25%40%25%40%25%40%25%40evil.com')).to eq('/')
-    end
-
-
-    it "considers %@%@%@%@%@%@%@%@%@%@evil.com an unsafe path" do
-      expect(Controller.safe_path('%@%@%@%@%@%@%@%@%@%@evil.com')).to eq('/')
-    end
-
-    it "considers https://www-bukalapak.com an unsafe path" do
-      expect(Controller.safe_path('https://www-bukalapak.com')).to eq('/')
+    UNSAFE_PATHS.each do |path|
+      it "considers #{path} an unsafe path" do
+        expect(Controller.safe_path(path)).to eq('/')
+      end
     end
 
     it "can use redirect_to method with only the target path" do
